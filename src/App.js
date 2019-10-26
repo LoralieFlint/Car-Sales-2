24d3221877c20e61496f2f33fe2763a94b2f9fad
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-25 18:43:25 -0600

    added but item functionality

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Header from './components/Header';
+import AddedFeatures from './components/AddedFeatures';
+import AdditionalFeatures from './components/AdditionalFeatures';
+import Total from './components/Total';
+
+import { connect } from "react-redux";
+
+const App = (props) => {
+  // const removeFeature = item => {
+  //   // dispatch an action here to remove an item
+  // };
+
+  // const buyItem = item => {
+  //   // dipsatch an action here to add an item
+  // };
+
+  return (
+    <div className="boxes">
+      <div className="box">
+        <Header car={props.state.car} />
+        <AddedFeatures car={props.state.car} />
+      </div>
+      <div className="box">
+        <AdditionalFeatures additionalFeatures={props.state.additionalFeatures} />
+        <Total car={props.state.car} additionalPrice={props.state.additionalPrice} />
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    state: state
+  }
+}
+
+export default connect(mapStateToProps, {}) (App);
---
+++ src/reducer/index.js
@@ -0,0 +1,49 @@
+const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 22990,
+      name: "2019 Kia Soul",
+      image:
+        "https://images.hgmsites.net/hug/2019-kia-soul_100658719_h.jpg",
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: "2.0L DOHC GDI 4-cylinder engine", price: 3000 },
+      { id: 3, name: "Deluxe sterio", price: 500 },
+      { id: 5, name: "Backup camera", price: 200 },
+    ]
+  };
+  
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "REMOVE_FEATURE":
+            return {
+                ...state,
+                car: {
+                    ...state.car,
+                    price: state.car.price - action.payload.price,
+                    features: state.car.features.filter(item => {
+                        return item.id !== action.payload.id
+                    })
+                }
+            }
+      case "BUY_FEATURE":
+        if (state.car.features.find(item => item.id === action.payload.id)) {
+          return state;
+        } else {
+          return {
+            ...state,
+            car: {
+              ...state.car,
+              price: state.car.price + action.payload.price,
+              features: [...state.car.features, action.payload]
+            }
+          };
+        }
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
